4b65f07553a2823d49642a384d2f6c41533e44b7
Matthew Sandytoes <msantos1@hcrhs.org>
2026-02-02 20:56:08 -0500

    1/2 working implementation of enums and stuff

--- src/main/cpp/subsystems/Intake.cpp
+++ src/main/cpp/subsystems/Intake.cpp
@@ -6,14 +6,24 @@ namespace IntakeConstants {
     int kArmMotorID = 2;
 
     double kP = 2.4;
-    double kI = 0;
-    double kD = 0.1;    
-
+    double kI = 0.0;
+    double kD = 0.1;   
+    double kG = 0.0;
+    double kS = 0.0;
+    double kV = 0.0;
+    
+    units::angle::turn_t armOutPos = 10_tr;
+    units::angle::turn_t armInPos = 0_tr;
+    units::angle::turn_t tolerance = 0.5_tr;
+    
+    units::angle::turn_t armPositions[] = {0_tr, 10_tr};
+    std::string_view armPositionNames[] = {"In", "Out"};
 
     units::volt_t intakeFowardVoltage = 6_V;
     units::volt_t intakeBackwardsVoltage = -7_V;
     units::volt_t armFowardVoltage = 6_V;
     units::volt_t armBackwardsVoltage = -7_V;
+
 }
 
 Intake::Intake() :
@@ -21,16 +31,26 @@ Intake::Intake() :
     m_armMotor{IntakeConstants::kArmMotorID, m_CANBusInstance},
     m_intakeMotor{IntakeConstants::kIntakeMotorID, m_CANBusInstance} 
 {
-    
+    ctre::phoenix6::configs::TalonFXConfiguration m_armConfig;
+
     ctre::phoenix6::configs::Slot0Configs slot0Configs{};
     slot0Configs.kP = IntakeConstants::kP; 
     slot0Configs.kI = IntakeConstants::kI; 
     slot0Configs.kD = IntakeConstants::kD; 
+    slot0Configs.kG = IntakeConstants::kG; 
+    slot0Configs.kS = IntakeConstants::kS; 
+    slot0Configs.kV = IntakeConstants::kV;
 
-    m_armMotor.GetConfigurator().Apply(slot0Configs);  
+    m_armConfig.WithSlot0(slot0Configs);
+
+    auto &motionMagicConfigs = m_armConfig.MotionMagic;
+    motionMagicConfigs.MotionMagicCruiseVelocity =
+      units::angular_velocity::turns_per_second_t{65};
+    motionMagicConfigs.MotionMagicAcceleration =
+      units::angular_acceleration::turns_per_second_squared_t{200};
+
+    m_armMotor.GetConfigurator().Apply(m_armConfig);  
     
-
-
 }
 
 Intake::~Intake() {
@@ -38,6 +58,18 @@ Intake::~Intake() {
 
 units::angle::turn_t Intake::GetArmPos() {
     return m_armMotor.GetPosition().GetValue();
+}
+
+bool Intake::IsArmOut() {
+    return IntakeConstants::armOutPos - IntakeConstants::tolerance < GetArmPos()
+        && IntakeConstants::armOutPos + IntakeConstants::tolerance > GetArmPos();
+}
+
+bool Intake::IsArmIn() {
+
+    return IntakeConstants::armInPos - IntakeConstants::tolerance < GetArmPos()
+        && IntakeConstants::armInPos + IntakeConstants::tolerance > GetArmPos();
+
 }
 
 void Intake::IntakeIn() {
