9c62a3be3bfc5d7acc8265450aed06223563952c
Daniel Denisov <git@danieldenisov.com>
2026-02-03 11:50:16 -0500

    shooter init

---
+++ src/main/include/subsystems/Shooter.h
@@ -0,0 +1,29 @@
+#include <ctre/phoenix6/TalonFX.hpp>
+#include <ctre/phoenix6/CANBus.hpp>
+#include <ctre/phoenix6/configs/Slot0Configs.hpp>
+#include <ctre/phoenix6/controls/VelocityVoltage.hpp>
+
+class Shooter {
+public:
+    Shooter();
+    ~Shooter();
+
+private:
+
+    void FeederIn();
+    void FeederOut();
+
+    void SetFlywheelSpeed(units::angular_velocity::turns_per_second_t velocity);
+    units::angular_velocity::turns_per_second_t targetVelocity{}; //read only
+    units::angular_velocity::turns_per_second_t GetCurrentFlywheelSpeed();
+    bool isAtCorrectSpeed();
+
+private:
+
+    ctre::phoenix6::CANBus m_CANBusInstance;
+
+    ctre::phoenix6::hardware::TalonFX m_flyWheelLeadMotor;
+    ctre::phoenix6::hardware::TalonFX m_flyWheelFollowMotor;
+
+    ctre::phoenix6::hardware::TalonFX m_feederMotor ;
+};
