ad3a5d40b3e189a73af8bd8ff890af152690d5cf
Daniel Denisov <git@danieldenisov.com>
2026-02-05 12:29:18 -0500

    added CmdPtr functions (not final)

--- src/main/include/subsystems/Intake.h
+++ src/main/include/subsystems/Intake.h
@@ -1,25 +1,41 @@
 #include <ctre/phoenix6/TalonFX.hpp>
 #include <ctre/phoenix6/CANBus.hpp>
 #include <ctre/phoenix6/configs/Slot0Configs.hpp>
+#include <frc2/command/CommandPtr.h>
+#include <frc2/command/Commands.h>
+#include <frc2/command/SubsystemBase.h>
 
-class Intake {
+class Intake : public frc2::SubsystemBase {
 public:
 
     Intake();
     ~Intake();
 
+public:
+    //Hangs until compleated
+    //Uses const voltage once out to keep out
+    frc2::CommandPtr GoArmOut();
+
+    //Hangs until compleated
+    frc2::CommandPtr GoArmIn();
+
+    //RunEnd
+    frc2::CommandPtr IntakeFuel();
+    
+    //RunEnd
+    frc2::CommandPtr OutakeFuel();
+
 private:
 
     units::angle::turn_t GetArmPos();
     
-    //void SetArmPos();
-    
     bool IsArmOut(); 
     bool IsArmIn();
-//Make a wrapper**
+
     void IntakeIn();
     void IntakeOut();
     void IntakeStop();
+    void IntakeConstVoltage();
     
     void ArmIn();
     void ArmOut();
