94d6e9b614122f145998e63ed6686a6ff129701c
Daniel Denisov <git@danieldenisov.com>
2026-02-19 15:02:40 -0500

    added feeder subsystem

---
+++ src/main/cpp/subsystems/shooter/Feeder.cpp
@@ -0,0 +1,92 @@
+#include "subsystems/shooter/Feeder.h"
+#include <rev/config/SparkMaxConfig.h>
+
+namespace FeederConstants {
+    int kTopMotorID = 21;
+    int kBottomMotorID = 22;
+
+    double kToleranceRPM = 100; //only used for setVelocityUntilSpeed
+
+    //Top Motor
+    units::volt_t TopMotorVoltageComp = 12_V;
+    units::ampere_t TopMotorSmartCurrentLimit = 40_A;
+    int TopMotor_P = 1.0;
+    int TopMotor_I = 0.0001;
+    int TopMotor_D = 0.0;
+    int TopMotor_FF = 0.1; 
+
+    //Bottom Motor
+    units::volt_t BottomMotorVoltageComp = 12_V;
+    units::ampere_t BottomMotorSmartCurrentLimit = 40_A;
+    int BottomMotor_P = 1.0;
+    int BottomMotor_I = 0.0001;
+    int BottomMotor_D = 0.0;
+    int BottomMotor_FF = 0.1; 
+
+    //For cleaner initialization of feeder class
+    struct Perams {
+        int motorID;
+        units::volt_t VoltComp;
+        units::ampere_t SmartCurrLim;
+        int kP, kI, kD, kFF;
+        
+        //True = Top Motor config, False = Bottom Motor config
+        Perams(bool isTopMotor) {
+            if (isTopMotor) {motorID = kTopMotorID; 
+                VoltComp = TopMotorVoltageComp; SmartCurrLim = TopMotorSmartCurrentLimit; 
+                kP = TopMotor_P; kI = TopMotor_I; kD = TopMotor_D; kFF = TopMotor_FF;
+            } else { motorID = kBottomMotorID; 
+                VoltComp = BottomMotorVoltageComp; SmartCurrLim = BottomMotorSmartCurrentLimit; 
+                kP = BottomMotor_P; kI = BottomMotor_I; kD = BottomMotor_D; kFF = BottomMotor_FF;
+            }
+        }
+    };
+
+}
+
+Feeder::Feeder(FeederConstants::Perams peramConfig) :
+    m_feederMotor(peramConfig.motorID, rev::spark::SparkFlex::MotorType::kBrushless),
+    m_pidController(m_feederMotor.GetClosedLoopController())    
+{
+    //velocty control (voltage but with some compensation)
+    rev::spark::SparkMaxConfig feederConfig = rev::spark::SparkMaxConfig();
+
+    feederConfig.VoltageCompensation(peramConfig.VoltComp.value());
+    feederConfig.SmartCurrentLimit(peramConfig.SmartCurrLim.value());
+
+    feederConfig.closedLoop
+    .P(peramConfig.kP)
+    .I(peramConfig.kI)
+    .D(peramConfig.kD)
+    .feedForward.kS(peramConfig.kFF); //maybe add KV?
+
+    m_feederMotor.Configure(feederConfig, 
+                rev::spark::SparkBase::ResetMode::kNoResetSafeParameters, 
+                rev::spark::SparkBase::PersistMode::kNoPersistParameters);
+
+}
+
+Feeder::~Feeder() {}
+
+frc2::CommandPtr Feeder::setRPM(double RPM) {
+    return Run([this, RPM] {setVelocity(RPM);});
+}
+
+frc2::CommandPtr Feeder::setRPM(double RPM) {
+    return setRPM(RPM).Until([this, RPM]() -> bool{return isAtRPM(RPM);});
+}
+
+double Feeder::getRPM() {
+    return m_feederMotor.GetEncoder().GetVelocity();
+}
+
+// ************************* Private ************************* //
+void Feeder::setVelocity(double RPM) {
+    targetRPM = RPM;
+    m_pidController.SetReference(RPM, rev::spark::SparkFlex::ControlType::kVelocity);
+}
+
+bool Feeder::isAtRPM(double RPM_Target) {
+    return RPM_Target - FeederConstants::kToleranceRPM < getRPM() &&
+            RPM_Target + FeederConstants::kToleranceRPM > getRPM();
+}
---
+++ src/main/include/subsystems/shooter/Feeder.h
@@ -0,0 +1,51 @@
+#include <ctre/phoenix6/TalonFX.hpp>
+#include <rev/SparkFlex.h>
+#include <rev/SparkClosedLoopController.h>
+#include <frc/DigitalInput.h>
+
+#include <frc2/command/CommandPtr.h>
+#include <frc2/command/Commands.h>
+#include <frc2/command/SubsystemBase.h>
+#include <frc2/command/Subsystem.h>
+#include <wpi/sendable/SendableBuilder.h>
+
+class FeederSim; //forward declarations
+namespace FeederConstants {
+    struct Perams; // Forward declaration
+}
+
+class Feeder : public frc2::SubsystemBase{
+public:
+    Feeder(FeederConstants::Perams peramConfig); //get the motor ID from constants and pass it in here
+    ~Feeder();
+
+public:
+    void InitializeDashboard();
+    void UpdateDashboard();
+    void Periodic() override;
+
+public:
+
+    frc2::CommandPtr setRPM(double RPM);
+    frc2::CommandPtr setRPMUntilThere(double RPM);
+    double getRPM();
+
+private:
+
+    void setVelocity(double RPM);
+    bool isAtRPM(double RPM);
+
+private:
+
+    rev::spark::SparkFlex m_feederMotor;
+    rev::spark::SparkClosedLoopController m_pidController;
+
+    double targetRPM = 0;
+
+// simulation
+private:
+    // friend class ShooterSim;
+    // std::unique_ptr<FeederSim> m_sim_state;
+
+    // void SimulationPeriodic() override;
+};
